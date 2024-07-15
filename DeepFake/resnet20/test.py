066266713997b82147ebfd0677f9869d106240b8
Asad Majeed <80450984+ASAD-BE18@users.noreply.github.com>
2024-07-16 01:58:02 +0500

    Update test.py

--- DeepFake/resnet20/test.py
+++ DeepFake/resnet20/test.py
@@ -43,7 +43,7 @@ def test_loop(dataloader, model, loss_fn, device):
 if __name__ == '__main__':
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     print('Device:', device)
-    save_dir = './saved'
+    save_dir = './DeepFake/resnet20/saved'
     data_dir = './Deepfake_dataset'
     batchsize = 128
 
