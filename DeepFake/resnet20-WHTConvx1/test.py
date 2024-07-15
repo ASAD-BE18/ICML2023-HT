7c7a49e494ba70d40a6d4111d05a95fb372f3a99
Asad Majeed <80450984+ASAD-BE18@users.noreply.github.com>
2024-07-16 02:43:56 +0500

    add whtconvx1

--- DeepFake/resnet20-WHTConvx1/test.py
+++ DeepFake/resnet20-WHTConvx1/test.py
@@ -43,7 +43,7 @@ def test_loop(dataloader, model, loss_fn, device):
 if __name__ == '__main__':
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     print('Device:', device)
-    save_dir = './DeepFake/resnet20-HTConvx3/saved'
+    save_dir = './DeepFake/resnet20-WHTConvx1/saved'
     data_dir = './Deepfake_dataset'
     batchsize = 128
 
