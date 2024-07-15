2862e52709e74d75172359d9cba370d4c43cd45a
Asad Majeed <80450984+ASAD-BE18@users.noreply.github.com>
2024-07-16 01:57:37 +0500

    Update train.py

--- DeepFake/resnet20/train.py
+++ DeepFake/resnet20/train.py
@@ -83,7 +83,7 @@ if __name__ == '__main__':
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     print('Device:', device)
-    save_dir = './saved'
+    save_dir = './DeepFake/resnet20/saved'
     data_dir = './Deepfake_dataset'
     batchsize = 128
     n_epochs = 200
