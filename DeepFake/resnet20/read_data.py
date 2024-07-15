72d9c2ae3c308bf43cd84ff35889edca6abed895
Asad Majeed <80450984+ASAD-BE18@users.noreply.github.com>
2024-07-16 01:46:07 +0500

    Update read_data.py

--- DeepFake/resnet20/read_data.py
+++ DeepFake/resnet20/read_data.py
@@ -34,12 +34,14 @@ class DeepfakeDataset(Dataset):
 def read_deepfake_dataset(batchsize, data_dir):
 
     transform_train = transforms.Compose([
+                                    transforms.Resize((32, 32)),  # Resize all images to 32x32
                                     transforms.RandomCrop(32, padding=4),  
                                     transforms.RandomHorizontalFlip(p=0.5), 
                                     transforms.ToTensor(),
                                     transforms.Normalize(mean=[0.4914, 0.4822, 0.4465],std=[0.2023, 0.1994, 0.2010])])
 
     transform_test = transforms.Compose([
+                                    transforms.Resize((32, 32)),  # Resize all images to 32x32
                                     transforms.ToTensor(),
                                     transforms.Normalize(mean=[0.4914, 0.4822, 0.4465],std=[0.2023, 0.1994, 0.2010])])
 
